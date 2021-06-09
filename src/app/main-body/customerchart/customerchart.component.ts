a8216ab0e37a7c456d4910d484364cf515ef5d3c
Abhishek <abhishekatholi@cet.ac.in>
2021-06-09 13:07:38 +0530

    customer table routing added

--- src/app/main-body/customerchart/customerchart.component.ts
+++ src/app/main-body/customerchart/customerchart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {CustomerService} from '../../services/customer.service'
-import {Chart} from 'chart.js'
+import { CustomerService } from '../../services/customer.service'
+import { Chart } from 'chart.js'
+import { DevicetestService } from '../../services/devicetest.service'
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-customerchart',
@@ -8,30 +10,265 @@ import {Chart} from 'chart.js'
   styleUrls: ['./customerchart.component.css']
 })
 export class CustomerchartComponent implements OnInit {
-  public customerList:any
+  chart: any
+  labels = []
+  deviceNum = []
+  testNum = []
+  dataset = []
 
-  constructor(private customerService:CustomerService) { }
-  getAllCustomer(){
-    this.customerService.getAllCustomer().subscribe((data:any)=>{
-      this.customerList=data
-      console.log(this.customerList,);
-      
-    })
-  }
+  constructor(
+    private customerService: CustomerService,
+    private devicetestService: DevicetestService
+  ) { }
+
   ngOnInit(): void {
-    var chart= new Chart('customerChart',{
-      type: 'line',
-      data: {
-        datasets: [{
-            type: 'bar',
-            label: 'Number of Devices',            
-            backgroundColor:'blue',
-            data :[10,20,30,40],
-            order: 1
-        }],
-        labels:['A','B','C','D']
-    }
-    })
+    this.initializeChart()
+
   }
 
+
+
+  initialChartConfig() {
+    return {
+      type: 'bar',
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            type: 'line',
+            yAxisID: 'testnumber',
+            data: this.testNum,
+            borderColor: 'red',
+            fill: false,
+            lineTension: 0,
+            borderWidth: 1,
+            pointBackgroundColor: 'red',
+            pointRadius: 2,
+
+
+          },
+          {
+            data: this.deviceNum,
+            yAxisID: 'devicenumber',
+            backgroundColor: '#0040ff',
+            barPercentage: .6,
+
+          }
+
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [
+            {
+              id: 'devicenumber',
+              type: 'linear',
+              position: 'left',
+              scaleLabel: {
+                labelString: 'Device Number',
+                display: true,
+                fontColor: 'blue',
+
+
+              },
+              gridLines: {
+
+
+              },
+              ticks: {
+                beginAtZero: true,
+                stepSize: 1
+              }
+            },
+            {
+              id: 'testnumber',
+              type: 'linear',
+              position: 'right',
+              scaleLabel: {
+                labelString: 'Test Conducted',
+                display: true,
+                fontColor: 'red'
+              },
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                beginAtZero: true,
+              }
+            },
+
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false
+              }
+            }
+          ]
+        }
+      }
+    }
+  }
+  // function to intialize chart
+  initializeChart() {
+    this.devicetestService.getTestData().subscribe((data: any) => {
+      console.log(data);
+      data.forEach(customer => {
+
+        let devicecount = 0
+        let testcount = 0
+        customer.data.forEach(device => {
+          devicecount += 1
+          device.data.forEach(year => {
+            year.testconducted.forEach(testNumber => {
+              testcount += testNumber
+            });
+          });
+        });
+        this.labels.push(customer.customer)
+        this.deviceNum.push(devicecount)
+        this.testNum.push(testcount)
+        this.dataset.push({ customer: customer.customer, deviceNum: devicecount, testNum: testcount })
+      });
+
+      console.log(this.labels, this.deviceNum, this.testNum);
+      console.log(this.dataset)
+
+      // chart configuration setup
+
+      this.chart = new Chart('customerChart', {
+        type: 'bar',
+        data: {
+          labels: this.labels,
+          datasets: [
+            {
+              type: 'line',
+              yAxisID: 'testnumber',
+              data: this.testNum,
+              borderColor: 'red',
+              fill: false,
+              lineTension: 0,
+              borderWidth: 1,
+              pointBackgroundColor: 'red',
+              pointRadius: 2,
+
+
+            },
+            {
+              data: this.deviceNum,
+              yAxisID: 'devicenumber',
+              backgroundColor: '#0040ff',
+              barPercentage: .6,
+
+            }
+
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          legend: {
+            display: false
+          },
+          scales: {
+            yAxes: [
+              {
+                id: 'devicenumber',
+                type: 'linear',
+                position: 'left',
+                scaleLabel: {
+                  labelString: 'Device Number',
+                  display: true,
+                  fontColor: 'blue',
+
+
+                },
+                gridLines: {
+
+
+                },
+                ticks: {
+                  beginAtZero: true,
+                  stepSize: 1
+                }
+              },
+              {
+                id: 'testnumber',
+                type: 'linear',
+                position: 'right',
+                scaleLabel: {
+                  labelString: 'Test Conducted',
+                  display: true,
+                  fontColor: 'red'
+                },
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  beginAtZero: true,
+                }
+              },
+
+            ],
+            xAxes: [
+              {
+                gridLines: {
+                  display: false
+                }
+              }
+            ]
+          }
+        }
+      })
+    })
+
+
+  }
+  //function for sort
+  sortByDevice() {
+
+    console.log(this.chart.data.labels);
+
+    this.dataset.sort((customer1, customer2) => (customer1.deviceNum > customer2.deviceNum ? -1 : 1))
+    let labels = []
+    let deviceNum = []
+    let testNum = []
+    this.dataset.forEach(element => {
+      deviceNum.push(element.deviceNum)
+      testNum.push(element.testNum)
+      labels.push(element.customer)
+    });
+    console.log(this.labels);
+
+    this.chart.data.labels = labels
+    this.chart.data.datasets[0].data = testNum
+    this.chart.data.datasets[1].data = deviceNum
+
+    this.chart.update()
+  }
+  sortByTest() {
+    console.log(this.dataset);
+    this.dataset.sort((customer1, customer2) => (customer1.testNum > customer2.testNum ? -1 : 1))
+    console.log(this.dataset);
+    let labels = []
+    let deviceNum = []
+    let testNum = []
+    this.dataset.forEach(element => {
+      deviceNum.push(element.deviceNum)
+      testNum.push(element.testNum)
+      labels.push(element.customer)
+    });
+    console.log(this.labels);
+
+    this.chart.data.labels = labels
+    this.chart.data.datasets[0].data = testNum
+    this.chart.data.datasets[1].data = deviceNum
+    this.chart.update()
+
+  }
 }
---
+++ src/app/main-body/customertable/customertable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customertable',
+  templateUrl: './customertable.component.html',
+  styleUrls: ['./customertable.component.css']
+})
+export class CustomertableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
